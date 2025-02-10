6bc61323f2b1d940ef92e99f3c9f748aac8ac686
David Howard <David.Howard@kcl.ac.uk>
2025-02-10 12:43:31 +0000

    Update 3_Create_Single_Ancestry_PCAs.sh

--- post_imputation/3_Create_Single_Ancestry_PCAs.sh
+++ post_imputation/3_Create_Single_Ancestry_PCAs.sh
@@ -2,39 +2,45 @@
 
 ## usage:
 ## $ module load plink2
-## $ ./2_Create_PCA.sh {filename}
+## $ ./3_Create_Single_Ancestry_PCAs.sh {filename}
 ## * note {filename} is the original name of your data without the _qc1 and .bed/.bim/.fam suffix 
 
+## Identify variants in linkage equilibrium with a maf > 0.05
 plink2 \
 --bfile $1_qc1 \
 --maf 0.05 \
 --indep-pairwise 200kb 0.5 \
 --out $1_qc1_indep
 
+## Use independent variants to create PCs
 plink2 \
 --bfile $1_qc1 \
 --extract $1_qc1_indep.prune.in \
 --pca 20 \
 --out $1_qc1_pca
 
+## Identify variants in linkage equilibrium with a maf > 0.05 in females
 plink2 \
 --bfile $1_qc1_female \
 --maf 0.05 \
 --indep-pairwise 200kb 0.5 \
 --out $1_qc1_female_indep
 
+## Use independent variants to create PCs in females
 plink2 \
 --bfile $1_qc1_female \
 --extract $1_qc1_female_indep.prune.in \
 --pca 20 \
 --out $1_qc1_female_pca
 
+## Identify variants in linkage equilibrium with a maf > 0.05 in males
 plink2 \
 --bfile $1_qc1_male \
 --maf 0.05 \
 --indep-pairwise 200kb 0.5 \
 --out $1_qc1_male_indep
 
+## Use independent variants to create PCs in males
 plink2 \
 --bfile $1_qc1_male \
 --extract $1_qc1_male_indep.prune.in \
