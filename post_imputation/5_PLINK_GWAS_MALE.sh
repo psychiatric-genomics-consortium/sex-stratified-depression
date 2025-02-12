2e6d448b2914dec85f7209137a990583cdb1c551
David Howard <David.Howard@kcl.ac.uk>
2025-02-12 14:28:50 +0000

    Create 5_PLINK_GWAS_MALE.sh

---
+++ post_imputation/5_PLINK_GWAS_MALE.sh
@@ -0,0 +1,15 @@
+## Script to run male only GWAS using PLINK2 logistic regression 
+## Sex should be in column 5 and the depression phenotype should be in column 6 of the .fam file
+## This file should be used when there was low relatedness in your sample and they were removed using the 2_QC_for_PLINK_GWAS.sh file
+
+## usage:
+## $ module load plink2
+## $ ./2_QC_for_PLINK_GWAS.sh {filename} {covariatefilename}
+## * note {filename} is the original name of your data without the .bed/.bim/.fam suffix. The {covariatefilename} is the full name of the file
+## containing the PCs identified as associated with depression using 4_Associated_PCAs.r along with age and any other relevant covariates
+
+plink2 \
+--bfile $1_qc1_male \
+--glm 'log10' cols=chrom,pos,ax,a1freqcc,totallele,totallelecc,machr2,firth,test,nobs,orbeta,se,p  \
+--covar $2 \
+--out $1_qc1_male_GWAS
