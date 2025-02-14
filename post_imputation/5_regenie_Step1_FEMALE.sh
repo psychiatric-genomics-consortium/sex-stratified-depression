69ec1feb54d1659697ffaa581bda03aa011fa6bb
David Howard <David.Howard@kcl.ac.uk>
2025-02-14 13:43:42 +0000

    Create 5_regenie_Step1_FEMALE.sh

---
+++ post_imputation/5_regenie_Step1_FEMALE.sh
@@ -0,0 +1,23 @@
+## Script to run step 1 of regenie using imputed data for females in bfile format
+## A depression phenotype file is created from column 6 of the .fam file. Cases = 2, Controls = 1
+## Only the previously identified variants in linkage equilibrium and with a maf > 0.05 are used in regenie step 1
+
+## usage:
+## $ module load regenie
+## $ ./5_regenie_Step1_FEMALE.sh {filename} {covariate filename}
+## * note {filename} is the original name of your data without the .bed/.bim/.fam suffix. The {covariatefilename} is the full name of the file
+## containing the PCs identified as associated with depression using 4_Associated_PCAs.r along with age and any other relevant covariates
+
+echo "FID IID pheno" > $1_qc1_female.pheno
+awk '{ print $1, $2, $6 }' $1_qc1_female.fam >> $1_qc1_female.pheno
+
+regenie \
+--step 1 \
+--bed $1_qc1_female \
+--extract $1_qc1_female_indep.prune.in \
+--covarFile $2 \
+--phenoFile $1_qc1_female.pheno \
+--cc12 \
+--bsize 100 \
+--bt \
+--out $1_qc1_regenie_step1_female
