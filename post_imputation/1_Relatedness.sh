314ce6c4f33768f697128fe1d326e449c30c6cd2
David Howard <David.Howard@kcl.ac.uk>
2025-02-26 12:34:56 +0000

    Update 1_Relatedness.sh

--- post_imputation/1_Relatedness.sh
+++ post_imputation/1_Relatedness.sh
@@ -1,5 +1,8 @@
 ## Script to identify second-degree related individuals within imputed data in bfile format using PLINK2
-## Sex should be in column 5 and the depression phenotype should be in column 6 of the .fam file
+## Sex should be in column 5
+## A three column depression phenotype file (Family ID, Individuals ID, depression status (control = 1, case = 2))
+## is expected and will need to have the same name as your imputed data with a .pheno suffix.
+
 ## The script will create a relationship matrix for the whole sample and then identify the related individuals
 ## usage:
 ## $ module load plink2
@@ -12,9 +15,11 @@
 
 plink2 \
 --bfile $1 \
+--pheno $1.pheno \
 --prune \
 --max-alleles 2 \
 --remove-nosex \
+--chr 1-22 \
 --maf 0.01 \
 --geno 0.1 \
 --mind 0.1 \
@@ -26,3 +31,6 @@ plink2 \
 --bfile $1 \
 --king-cutoff $1_king 0.125 \
 --out $1_king_unrel
+
+rm $1_king.king.*
+rm $1_king_unrel.king.cutoff.in.id
