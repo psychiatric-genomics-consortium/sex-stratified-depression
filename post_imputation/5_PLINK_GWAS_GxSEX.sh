1ad060407b52fd1c94c7020c205dce1e33cded08
David Howard <David.Howard@kcl.ac.uk>
2025-02-26 12:43:42 +0000

    Update 5_PLINK_GWAS_GxSEX.sh

--- post_imputation/5_PLINK_GWAS_GxSEX.sh
+++ post_imputation/5_PLINK_GWAS_GxSEX.sh
@@ -1,5 +1,4 @@
-## Script to run Genotype X Sex interation GWAS using PLINK2 logistic regression 
-## Depression phenotype should be in column 6 of the .fam file
+## Script to run Genotype X Sex interation GWAS using PLINK2 logistic regression
 ## This file should be used when there was low relatedness in your sample and they were removed using the 2_QC_for_PLINK_GWAS.sh file
 
 ## THIS CODE WILL NEED THE PARAMETERS LINE EDITED BASED ON NUMBER OF COVARIATES AND POSITION OF sex IN YOUR COVARIATE FILE
@@ -15,7 +14,10 @@
 ## containing the PCs identified as associated with depression using 4_Associated_PCAs.r along with sex, age and any other relevant covariates
 
 plink2 \
---bfile $1_qc1 \
+--bfile $1 \
+--pheno $1.pheno \
+--keep $1_qc1.id \
+--extract $1_qc1.snplist \
 --glm interaction 'log10' cols=chrom,pos,ax,a1freqcc,totallele,totallelecc,machr2,firth,test,nobs,orbeta,se,p  \
 --covar $2 \
 --parameters 1-{ncovar + 1}, {sexcovar + ncovar + 1} \
