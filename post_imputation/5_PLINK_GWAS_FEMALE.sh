896a6c47b2e43144670713d4e075d61cb447bfb2
David Howard <David.Howard@kcl.ac.uk>
2025-06-20 15:40:53 +0100

    Update 5_PLINK_GWAS_FEMALE.sh

--- post_imputation/5_PLINK_GWAS_FEMALE.sh
+++ post_imputation/5_PLINK_GWAS_FEMALE.sh
@@ -3,15 +3,16 @@
 
 ## usage:
 ## $ module load plink2
-## $ ./5_PLINK_GWAS_FEMALE.sh {filename} {covariatefilename}
-## * note {filename} is the original name of your data without the .bed/.bim/.fam suffix. The {covariatefilename} is the full name of the file
-## containing the sex-specific PCs identified as associated with depression using 4_Associated_PCAs.r along with age and any other relevant covariates
+## $ ./sex-stratified-depression/post_imputation/5_PLINK_GWAS_FEMALE.sh filename filename_qc1_female_PCA.covar
+
+## filename is the original name of your data without the .bed/.bim/.fam suffix. After the filename, the covariate filename is also required and 
+## if you have retained the covariate filenames from Step 4, then you will just need to update the filename prefix.
 
 plink2 \
 --bfile $1 \
 --pheno $1.pheno \
 --keep $1_qc1_female.id \
 --extract $1_qc1_female.snplist \
---glm 'log10' cols=chrom,pos,ax,a1freqcc,totallele,totallelecc,machr2,firth,test,nobs,orbeta,se,p  \
+--glm 'log10' cols=chrom,pos,omitted,a1freqcc,totallele,totallelecc,machr2,firth,test,nobs,orbeta,se,p  \
 --covar $2 \
 --out $1_qc1_female_GWAS
