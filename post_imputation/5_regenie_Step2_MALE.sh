86d8f75f1006e8d5a890cafaa363d7e0d8887bbc
David Howard <David.Howard@kcl.ac.uk>
2025-08-22 10:57:44 +0100

    Update 5_regenie_Step2_MALE.sh

--- post_imputation/5_regenie_Step2_MALE.sh
+++ post_imputation/5_regenie_Step2_MALE.sh
@@ -3,7 +3,7 @@
 
 ## usage:
 ## $ module load regenie
-## $ ./5_regenie_Step2_MALE.sh filename filename_qc1_male_PCA.covar
+## $ ./5_regenie_Step2_MALE.sh filename filename_qc_male_PCA.covar
 ## *note filename is the name of your data without the .bed/.bim/.fam suffix. 
 ## After the filename, the covariate filename is also required and if you have retained the covariate filenames from Step 4,
 ## then you will only need to update the filename prefix.
@@ -11,8 +11,8 @@
 regenie \
 --step 2 \
 --bed $1 \
---keep $1_qc1_male.id \
---extract $1_qc1_male.snplist \
+--keep $1_qc_male.id \
+--extract $1_qc_male.snplist \
 --covarFile $2 \
 --phenoFile $1.pheno \
 --cc12 \
@@ -21,5 +21,5 @@ regenie \
 --af-cc \
 --firth \
 --pThresh 0.05 \
---pred $1_qc1_male_regenie_step1_pred.list \
---out $1_qc1_male_regenie_firth
+--pred $1_qc_male_regenie_step1_pred.list \
+--out $1_qc_male_regenie_firth
