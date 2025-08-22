7117741ff1cd86183fbb439c857975660d2cf9d3
David Howard <David.Howard@kcl.ac.uk>
2025-08-22 10:56:00 +0100

    Update 5_regenie_Step1_GxSEX.sh

--- post_imputation/5_regenie_Step1_GxSEX.sh
+++ post_imputation/5_regenie_Step1_GxSEX.sh
@@ -3,7 +3,7 @@
 
 ## usage:
 ## $ module load regenie
-## $ ./5_regenie_Step1_GxSEX.sh filename filename_qc1_PCA.covar
+## $ ./5_regenie_Step1_GxSEX.sh filename filename_qc_PCA.covar
 ## *note filename is the name of your data without the .bed/.bim/.fam suffix. 
 ## After the filename, the covariate filename is also required and if you have retained the covariate filenames from Step 4,
 ## then you will only need to update the filename prefix.
@@ -11,11 +11,11 @@
 regenie \
 --step 1 \
 --bed $1 \
---keep $1_qc1.id \
---extract $1_qc1_geno.prune.in \
+--keep $1_qc.id \
+--extract $1_qc_geno.prune.in \
 --covarFile $2 \
 --phenoFile $1.pheno \
 --cc12 \
 --bsize 100 \
 --bt \
---out $1_qc1_regenie_step1_GxSEX
+--out $1_qc_regenie_step1_GxSEX
