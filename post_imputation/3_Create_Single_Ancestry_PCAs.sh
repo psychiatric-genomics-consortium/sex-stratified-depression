ec1c6d56b350240c29682ef18778234461c13cd5
David Howard <David.Howard@kcl.ac.uk>
2025-08-22 10:52:38 +0100

    Update 3_Create_Single_Ancestry_PCAs.sh

--- post_imputation/3_Create_Single_Ancestry_PCAs.sh
+++ post_imputation/3_Create_Single_Ancestry_PCAs.sh
@@ -3,7 +3,7 @@
 ## usage:
 ## $ module load plink2
 ## $ ./3_Create_Single_Ancestry_PCAs.sh filename
-## * note filename is the name of your data without the _qc1 and .bed/.bim/.fam suffix
+## * note filename is the name of your data without the _qc and .bed/.bim/.fam suffix
 
 ## create file of high LD regions to be excluded for PCA
 echo "5 44000000 51500000 r1" > highld_for_exc.txt
@@ -14,53 +14,53 @@ echo "11 45000000 57000000 r4" >> highld_for_exc.txt
 ## Identify variants in linkage equilibrium with a maf > 0.05
 plink2 \
 --bfile $1 \
---keep $1_qc1.id \
+--keep $1_qc.id \
 --exclude range highld_for_exc.txt \
---extract $1_qc1.snplist \
+--extract $1_qc.snplist \
 --maf 0.05 \
 --indep-pairwise 200kb 0.5 \
---out $1_qc1_indep
+--out $1_qc_indep
 
 ## Use independent variants to create PCs
 plink2 \
 --bfile $1 \
---keep $1_qc1.id \
---extract $1_qc1_indep.prune.in \
+--keep $1_qc.id \
+--extract $1_qc_indep.prune.in \
 --pca 20 \
---out $1_qc1_pca
+--out $1_qc_pca
 
 ## Identify variants in linkage equilibrium with a maf > 0.05 in females
 plink2 \
 --bfile $1 \
---keep $1_qc1_female.id \
+--keep $1_qc_female.id \
 --exclude range highld_for_exc.txt \
---extract $1_qc1_female.snplist \
+--extract $1_qc_female.snplist \
 --maf 0.05 \
 --indep-pairwise 200kb 0.5 \
---out $1_qc1_female_indep
+--out $1_qc_female_indep
 
 ## Use independent variants to create PCs in females
 plink2 \
 --bfile $1 \
---keep $1_qc1_female.id \
---extract $1_qc1_female_indep.prune.in \
+--keep $1_qc_female.id \
+--extract $1_qc_female_indep.prune.in \
 --pca 20 \
---out $1_qc1_female_pca
+--out $1_qc_female_pca
 
 ## Identify variants in linkage equilibrium with a maf > 0.05 in males
 plink2 \
 --bfile $1 \
---keep $1_qc1_male.id \
+--keep $1_qc_male.id \
 --exclude range highld_for_exc.txt \
---extract $1_qc1_male.snplist \
+--extract $1_qc_male.snplist \
 --maf 0.05 \
 --indep-pairwise 200kb 0.5 \
---out $1_qc1_male_indep
+--out $1_qc_male_indep
 
 ## Use independent variants to create PCs in males
 plink2 \
 --bfile $1 \
---keep $1_qc1_male.id \
---extract $1_qc1_male_indep.prune.in \
+--keep $1_qc_male.id \
+--extract $1_qc_male_indep.prune.in \
 --pca 20 \
---out $1_qc1_male_pca
+--out $1_qc_male_pca
